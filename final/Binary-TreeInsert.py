b5008354433b09569035e417e778fa15402faa70
thitimon171143 <montha.kam.iam@gmail.com>
2020-01-28 15:56:28 +0700

    hard

--- final/Binary-TreeInsert.py
+++ final/Binary-TreeInsert.py
@@ -1,6 +1,6 @@
 class Node:
         def __init__(self, data):
-            self.leaf = None
+            self.left = None
             self.right = None
             self.data = data
 
@@ -10,7 +10,7 @@ class Node:
                         if self.left is None:
                             self.left = Node(data)
                         else:
-                            self.leaf.insert(data)
+                            self.left.insert(data)
                     elif data > self.data:
                         if self.right is None:
                             self.right = Node(data)
@@ -25,10 +25,56 @@ class Node:
             print(self.data),
             if self.right:
                 self.right.PrintTree()
+        
+        def findval(self, lkpval):
+            if lkpval < self.data:
+                if self.left is None:
+                    return str(lkpval)+" Not Found"
+                return self.left.findval(lkpval)
+            elif lkpval > self.data:
+                if self.right is None:
+                    return str(lkpval)+"Not Found"
+                return self.right.findval(lkpval)
+            else:
+                print(str(self.data)+ ' is found')
+        def inorderTraversal(self, root):
+            res = []
+            if root:
+                res  = self.inorderTraversal(root.left)
+                res.append(root.data)
+                res = res + self.inorderTraversal(root.right)
+            return res
+        def PostorderTraversal(self,root):
+            res = []
+            if root:
+                res = self.PostorderTraversal(root.left)
+                res = res+self.PostorderTraversal(root.right)
+                res.append(root.data)
+            return res
+        def PreorderTraversal(self, root):
+            res = []
+            if root:
+                res.append(root.data)
+                res = res + self.PreorderTraversal(root.left)
+                res = res + self.PreorderTraversal(root.right)
+            return res
 
-root = Node(12)
-root.insert(6)
-root.insert(14)
-root.insert(3)
+        def Deletion(self,root):
+            res = []
+            if root:
+                res.append(root.data)
+                res = res + self.PreorderTraversal(root.left)
+                res = res + self.PreorderTraversal(root.right)
+                res.pop(-2)
+            return res
 
-root.PrintTree()
+root = Node(100)
+root.insert(150)
+root.insert(20)
+root.insert(50)
+root.insert(60)
+
+print(root.inorderTraversal(root))
+print(root.PostorderTraversal(root))
+print(root.PreorderTraversal(root))
+print(root.Deletion(root))
